1ebec95b9b774642e397db8d0c0a38367a8d24ab
Khachaporn2546 <s6502041620018@email.kmutnb.ac.th>
2025-01-27 14:20:31 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Khachaporn 
+          Khachaporn Lunthaiyo
         </p>
         <a
           className="App-link"
